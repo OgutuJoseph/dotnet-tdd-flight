71d357a569b977a1fd8f1a9b900a17107ec556ff
OgutuJoseph <onyango.joseph@yahoo.com>
2023-09-27 11:47:44 +0300

    Translating a scenario to test

---
+++ Flight/Domain.Tests/Flight.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Tests
+{
+	public class Flight
+	{
+        public Flight(int seatCapacity)
+		{ 
+			RemainingNumberOfSeats = seatCapacity;
+		}
+
+		public void Book(string v1, int v2)
+		{
+			RemainingNumberOfSeats -= v2;
+		}
+
+		public int RemainingNumberOfSeats { get; set; }
+	}
+}
--- Flight/Flight/UnitTest1.cs
+++ Flight/Flight/UnitTest1.cs
@@ -1,11 +1,18 @@
-namespace Flight
+using Domain.Tests;
+using FluentAssertions;
+
+namespace FlightTest
 {
 	public class UnitTest1
 	{
 		[Fact]
 		public void Test1()
 		{
+			var flight = new Flight(seatCapacity: 3);
 
+			flight.Book("ojdev@mail.com", 1);
+
+			flight.RemainingNumberOfSeats.Should().Be(2);		
 		}
 	}
 }
